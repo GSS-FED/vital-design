3831d3435fe48ab2762d8df28336d0af46ff2cf9
Arthur Lu <arthur_lu@gss.com.tw>
2023-10-03 14:35:02 +0800

    chore: 啟用 Vite 的 library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      formats: ['es'],
+    },
+  },
   plugins: [
     react({
       babel: {
