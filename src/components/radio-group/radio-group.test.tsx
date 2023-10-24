191c8a7742c59e63884c52421c1e94477b75edf5
Arthur Lu <arthur_lu@gss.com.tw>
2023-10-24 14:49:00 +0800

    test: 新增 `RadioGroup` 元件的測試

---
+++ src/components/radio-group/radio-group.test.tsx
@@ -0,0 +1,136 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { expect, it, vi } from 'vitest';
+import RadioGroup from './radio-group';
+
+it('renders a radio button', () => {
+  const props = {
+    options: [{ label: 'Option 1', value: 'option-1' }],
+    value: '',
+    onChange: vi.fn(),
+  };
+  render(<RadioGroup {...props} />);
+  const radioButton = screen.getByLabelText('Option 1');
+  expect(radioButton).toBeInTheDocument();
+  expect(radioButton).toHaveValue('option-1');
+  expect(radioButton).toHaveAttribute('data-state', 'unchecked');
+});
+
+it('renders a checked radio button', () => {
+  const props = {
+    options: [{ label: 'Option 1', value: 'option-1' }],
+    value: 'option-1',
+    onChange: vi.fn(),
+  };
+  render(<RadioGroup {...props} />);
+  const radioButton = screen.getByLabelText('Option 1');
+  expect(radioButton).toBeInTheDocument();
+  expect(radioButton).toHaveValue('option-1');
+  expect(radioButton).toHaveAttribute('data-state', 'checked');
+});
+
+it('renders a disabled radio button', () => {
+  const props = {
+    options: [
+      { label: 'Option 1', value: 'option-1', disabled: true },
+    ],
+    value: '',
+    onChange: vi.fn(),
+  };
+  render(<RadioGroup {...props} />);
+  const radioButton = screen.getByLabelText('Option 1');
+  expect(radioButton).toBeInTheDocument();
+  expect(radioButton).toHaveValue('option-1');
+  expect(radioButton).toHaveAttribute('data-state', 'unchecked');
+});
+
+it('renders a disabled radio button which is also checked', () => {
+  const props = {
+    options: [
+      { label: 'Option 1', value: 'option-1', disabled: true },
+    ],
+    value: 'option-1',
+    onChange: vi.fn(),
+  };
+  render(<RadioGroup {...props} />);
+  const radioButton = screen.getByLabelText('Option 1');
+  expect(radioButton).toBeInTheDocument();
+  expect(radioButton).toHaveValue('option-1');
+  expect(radioButton).toHaveAttribute('data-state', 'checked');
+});
+
+it('renders multiple radio buttons', () => {
+  const props = {
+    options: [
+      { label: 'Option 1', value: 'option-1' },
+      { label: 'Option 2', value: 'option-2' },
+      { label: 'Option 3', value: 'option-3', disabled: true },
+    ],
+    value: 'option-1',
+    onChange: vi.fn(),
+  };
+  render(<RadioGroup {...props} />);
+  props.options.forEach((option) => {
+    const radioButton = screen.getByLabelText(option.label);
+    expect(radioButton).toBeInTheDocument();
+  });
+});
+
+it('calls the onChange callback when an unchecked radio button is clicked', async () => {
+  const props = {
+    options: [
+      { label: 'Option 1', value: 'option-1' },
+      { label: 'Option 2', value: 'option-2' },
+    ],
+    value: 'option-1',
+    onChange: vi.fn(),
+  };
+  render(<RadioGroup {...props} />);
+  props.options.forEach((option) => {
+    const radioButton = screen.getByLabelText(option.label);
+    expect(radioButton).toBeInTheDocument();
+  });
+  const secondRadioButton = screen.getByLabelText('Option 2');
+  await userEvent.click(secondRadioButton);
+  expect(props.onChange).toHaveBeenCalledWith('option-2');
+});
+
+it('does not call the onChange callback when a disabled radio button is clicked', async () => {
+  const props = {
+    options: [
+      { label: 'Option 1', value: 'option-1' },
+      { label: 'Option 2', value: 'option-2', disabled: true },
+    ],
+    value: 'option-1',
+    onChange: vi.fn(),
+  };
+  render(<RadioGroup {...props} />);
+  props.options.forEach((option) => {
+    const radioButton = screen.getByLabelText(option.label);
+    expect(radioButton).toBeInTheDocument();
+  });
+  const secondRadioButton = screen.getByLabelText('Option 2');
+  await userEvent.click(secondRadioButton);
+  expect(props.onChange).not.toHaveBeenCalled();
+});
+
+it('applies custom class names and styles', () => {
+  const props = {
+    options: [
+      { label: 'Option 1', value: 'option-1' },
+      { label: 'Option 2', value: 'option-2' },
+    ],
+    value: 'option-1',
+    onChange: vi.fn(),
+    className: 'custom-class-1 custom-class-2',
+    style: {
+      color: '#FED655',
+      backgroundColor: '#655FED',
+    },
+  };
+  render(<RadioGroup {...props} />);
+  const radioGroup = screen.getByRole('radiogroup');
+  expect(radioGroup).toBeInTheDocument();
+  expect(radioGroup).toHaveClass(props.className);
+  expect(radioGroup).toHaveStyle(props.style);
+});
