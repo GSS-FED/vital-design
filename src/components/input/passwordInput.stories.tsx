448329bc59350b8bde384fb226770a072fe91d01
annie_chien <annie_chien@gss.com.tw>
2024-10-14 15:27:50 +0800

    feat: 實作 PasswordInput 元件

---
+++ src/components/input/passwordInput.stories.tsx
@@ -0,0 +1,92 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import UserIcon from '../../icons/user-icon';
+import PasswordInput from './passwordInput';
+
+const meta: Meta<typeof PasswordInput> = {
+  title: 'Components/Input/PasswordInput',
+  component: PasswordInput,
+  tags: ['autodocs'],
+
+  //TODO: 不知道為什麼這邊的 argTypes 不會自動產，所以先手動加
+  argTypes: {
+    width: {
+      table: {
+        defaultValue: { summary: `100%` },
+      },
+    },
+    prefix: {},
+    value: {
+      type: { name: 'string', required: true },
+    },
+    initiallyVisible: {
+      table: {
+        defaultValue: { summary: false },
+      },
+    },
+    disabled: {
+      table: {
+        defaultValue: { summary: false },
+      },
+    },
+    onChange: {
+      type: { name: 'function', required: true },
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof PasswordInput>;
+
+export const Basic: Story = {
+  render() {
+    function App() {
+      const [value, setValue] = useState('');
+
+      return (
+        <PasswordInput
+          placeholder="請輸入密碼"
+          value={value}
+          onChange={setValue}
+        />
+      );
+    }
+    return <App />;
+  },
+};
+
+export const Prefix: Story = {
+  render() {
+    function App() {
+      const [value, setValue] = useState('');
+
+      return (
+        <PasswordInput
+          placeholder="請輸入密碼"
+          prefix={<UserIcon />}
+          value={value}
+          onChange={setValue}
+        />
+      );
+    }
+    return <App />;
+  },
+};
+
+export const Disabled: Story = {
+  render() {
+    function App() {
+      const [value, setValue] = useState('');
+
+      return (
+        <PasswordInput
+          placeholder="請輸入密碼"
+          value={value}
+          onChange={setValue}
+          disabled={true}
+        />
+      );
+    }
+    return <App />;
+  },
+};
