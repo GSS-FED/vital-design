922aaefbe6b687440897026cdb8bc448b74fb0f2
annie_chien <annie_chien@gss.com.tw>
2024-11-28 16:18:11 +0800

    chore: 更新 storybook 到 v8 並調整設定檔

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,23 +2,32 @@ import { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+
   stories: [
     '../src/**/*.mdx',
     '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
+
   docs: {
     autodocs: 'tag',
     defaultName: 'Documentation',
   },
+
+  //Configures how Storybook handles TypeScript files
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
+
   async viteFinal(config) {
     const { plugins, ...rest } = config;
     const mergedConfig = mergeConfig(
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,10 +2,13 @@ import { Preview } from '@storybook/react';
 
 const preview: Preview = {
   parameters: {
-    actions: {
-      argTypesRegex: '^on[A-Z].*',
+    controls: { exclude: ['style', 'className'] },
+    docs: {
+      toc: true, //Enables the table of contents
     },
   },
+  //Enable auto-generated documentation for all stories
+  tags: ['autodocs'],
 };
 
 export default preview;
