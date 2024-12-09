160c98d82ec09109c6821b6c9f46c9593e2a1103
Joanna Wang <joanna_wang@gss.com.tw>
2024-12-09 11:40:39 +0800

    refactor: 重構 Switch 元件的 story

--- src/components/switch/switch.stories.tsx
+++ src/components/switch/switch.stories.tsx
@@ -1,66 +1,28 @@
+import { useArgs } from '@storybook/preview-api';
 import { Meta, StoryObj } from '@storybook/react';
-import { useState } from 'react';
-import Switch from './switch';
+import { fn } from '@storybook/test';
+import Switch, { SwitchProps } from './switch';
 
 type Story = StoryObj<typeof Switch>;
 
 const meta: Meta<typeof Switch> = {
   title: 'Components/Switch',
   component: Switch,
-  tags: ['autodocs'],
+  args: {
+    onChange: fn(),
+    disabled: false,
+    checked: false,
+  },
 };
 export default meta;
 
-export const Unchecked: Story = {
-  render() {
-    function App() {
-      const [isChecked, setIsChecked] = useState(false);
-      return <Switch checked={isChecked} onChange={setIsChecked} />;
-    }
-    return <App />;
-  },
-};
+export const Default: Story = {
+  render: function Render(args) {
+    const [{ checked }, updateArgs] = useArgs<SwitchProps>();
+    const onChange = (isChecked: boolean) => {
+      updateArgs({ checked: isChecked });
+    };
 
-export const Checked: Story = {
-  render() {
-    function App() {
-      const [isChecked, setIsChecked] = useState(true);
-      return <Switch checked={isChecked} onChange={setIsChecked} />;
-    }
-    return <App />;
-  },
-};
-
-export const UncheckedAndDisabled: Story = {
-  name: 'Unchecked & Disabled',
-  render() {
-    function App() {
-      const [isChecked, setIsChecked] = useState(false);
-      return (
-        <Switch
-          disabled
-          checked={isChecked}
-          onChange={setIsChecked}
-        />
-      );
-    }
-    return <App />;
-  },
-};
-
-export const CheckedAndDisabled: Story = {
-  name: 'Checked & Disabled',
-  render() {
-    function App() {
-      const [isChecked, setIsChecked] = useState(true);
-      return (
-        <Switch
-          disabled
-          checked={isChecked}
-          onChange={setIsChecked}
-        />
-      );
-    }
-    return <App />;
+    return <Switch {...args} onChange={onChange} checked={checked} />;
   },
 };
