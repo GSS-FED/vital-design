6bed7dc53db771ccec557a7bad807267abeebeab
Joanna Wang <joanna_wang@gss.com.tw>
2024-12-09 11:39:33 +0800

    refactor: 重構 Radio-Group 元件的 story

--- src/components/radio-group/radio-group.stories.tsx
+++ src/components/radio-group/radio-group.stories.tsx
@@ -1,132 +1,64 @@
+import { useArgs } from '@storybook/preview-api';
 import { Meta, StoryObj } from '@storybook/react';
-import { useState } from 'react';
-import RadioGroup from './radio-group';
+import { fn } from '@storybook/test';
+import RadioGroup, { RadioGroupProps } from './radio-group';
 
 type Story = StoryObj<typeof RadioGroup>;
 
 const meta: Meta<typeof RadioGroup> = {
   title: 'Components/RadioGroup',
   component: RadioGroup,
-  tags: ['autodocs'],
+  argTypes: {
+    direction: {
+      options: ['horizontal', 'vertical'],
+      control: { type: 'select' },
+      defaultValue: { summary: 'horizontal' },
+    },
+  },
+  args: {
+    onChange: fn(),
+    direction: 'horizontal',
+    options: [
+      { label: '選項一', value: 'unique-value-a', disabled: false },
+      { label: '選項二', value: 'unique-value-b', disabled: false },
+      { label: '選項三', value: 'unique-value-c', disabled: false },
+    ],
+  },
 };
 export default meta;
 
-export const Horizontal: Story = {
-  render() {
-    function App() {
-      const [value, setValue] = useState('');
-      return (
-        <RadioGroup
-          options={[
-            { label: '選項一', value: 'unique-value-a' },
-            { label: '選項二', value: 'unique-value-b' },
-            { label: '選項三', value: 'unique-value-c' },
-          ]}
-          value={value}
-          onChange={setValue}
-        />
-      );
-    }
-    return <App />;
-  },
-};
+export const Default: Story = {
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs<RadioGroupProps>();
+    const onChange = (checkedValue: string) => {
+      updateArgs({ value: checkedValue });
+    };
 
-export const Vertical: Story = {
-  render() {
-    function App() {
-      const [value, setValue] = useState('');
-      return (
-        <RadioGroup
-          direction="vertical"
-          options={[
-            {
-              label: '選項一',
-              value: 'unique-value-a',
-            },
-            {
-              label: '選項二',
-              value: 'unique-value-b',
-            },
-            {
-              label: `${'很長'.repeat(50)}的選項`,
-              value: 'unique-value-c',
-            },
-          ]}
-          value={value}
-          onChange={setValue}
-        />
-      );
-    }
-    return <App />;
-  },
-};
-
-export const Unchecked: Story = {
-  render() {
-    function App() {
-      const [value, setValue] = useState('');
-      return (
-        <RadioGroup
-          options={[{ label: '選項', value: 'unique-value' }]}
-          value={value}
-          onChange={setValue}
-        />
-      );
-    }
-    return <App />;
-  },
-};
-
-export const Checked: Story = {
-  render() {
-    function App() {
-      const [value, setValue] = useState('unique-value');
-      return (
-        <RadioGroup
-          options={[{ label: '選項', value: 'unique-value' }]}
-          value={value}
-          onChange={setValue}
-        />
-      );
-    }
-    return <App />;
-  },
-};
-
-export const UncheckedAndDisabled: Story = {
-  name: 'Unchecked & Disabled',
-  render() {
-    function App() {
-      const [value, setValue] = useState('');
-      return (
-        <RadioGroup
-          options={[
-            { label: '選項', value: 'unique-value', disabled: true },
-          ]}
-          value={value}
-          onChange={setValue}
-        />
-      );
-    }
-    return <App />;
+    return <RadioGroup {...args} onChange={onChange} value={value} />;
   },
 };
 
 export const CheckedAndDisabled: Story = {
   name: 'Checked & Disabled',
-  render() {
-    function App() {
-      const [value, setValue] = useState('unique-value');
-      return (
-        <RadioGroup
-          options={[
-            { label: '選項', value: 'unique-value', disabled: true },
-          ]}
-          value={value}
-          onChange={setValue}
-        />
-      );
-    }
-    return <App />;
+  args: {
+    options: [
+      { label: '選項', value: 'unique-value', disabled: true },
+    ],
+    value: 'unique-value',
+  },
+};
+
+export const LongTextOption: Story = {
+  name: 'Option With Long Text',
+  args: {
+    direction: 'vertical',
+    options: [
+      {
+        label: `${'很長'.repeat(50)}的選項一`,
+        value: 'unique-value-a',
+      },
+      { label: '選項二', value: 'unique-value-b' },
+      { label: '選項三', value: 'unique-value-c' },
+    ],
   },
 };
