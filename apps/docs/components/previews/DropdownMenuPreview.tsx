59067e208e3a13c9943269654d020f7f2f201cef
dover_shen <dover_shen@gss.com.tw>
2026-07-17 18:46:31 +0800

    docs(dropdown-menu): 補 portalContainer 文件與 submenu 示範
    新增 Content props 表（含 portalContainer），並補 Submenu 的
    Storybook story 與 Fumadocs 互動 preview。

--- apps/docs/components/previews/DropdownMenuPreview.tsx
+++ apps/docs/components/previews/DropdownMenuPreview.tsx
@@ -8,6 +8,9 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from '@/components/dropdown-menu/DropdownMenu';
 import { ComponentPreview } from '~/components/preview/ComponentPreview';
@@ -26,6 +29,33 @@ export function DropdownMenuPreview() {
           </DropdownMenuGroup>
           <DropdownMenuSeparator />
           <DropdownMenuItem>Log out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </ComponentPreview>
+  );
+}
+
+export function DropdownMenuSubmenuPreview() {
+  return (
+    <ComponentPreview name="DropdownMenuSubmenuPreview">
+      <DropdownMenu>
+        <DropdownMenuTrigger render={<Button>File</Button>} />
+        <DropdownMenuContent>
+          <DropdownMenuItem>New file</DropdownMenuItem>
+          <DropdownMenuItem>Open…</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              Open recent
+            </DropdownMenuSubTrigger>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem>project-a.tsx</DropdownMenuItem>
+              <DropdownMenuItem>project-b.tsx</DropdownMenuItem>
+              <DropdownMenuItem>notes.md</DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuSub>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Settings</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     </ComponentPreview>
